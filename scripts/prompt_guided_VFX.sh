0e2bf8b157606a5162d8c4ebf67a693dba1ebc69
fangyuanmao <fangyuanmaocs@gmail.com>
2025-08-12 14:08:29 +0800

    Update prompt_guided_VFX.sh

--- scripts/prompt_guided_VFX.sh
+++ scripts/prompt_guided_VFX.sh
@@ -1,7 +1,7 @@
 python prompt_guided_VFX.py \
     --prompt "Character is surrounded by puppies, Turn it into 3D cartoon style." \
     --image_or_video_path "/path/to/ref_image" \
-    --model_path checkpoints/CogVideoX1.5-5B-I2V-Omni-VFX \
+    --model_path checkpoints/CogVideoX1.5-5B-I2V-OmniVFX \
     --output_path VFX.mp4 \
     --num_frames 81 \
     --width 1360 \
